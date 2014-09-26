c0c8641018249a7eb78cda30c3b649735f64eb6f
IljaGoushcha <iljagou@yahoo.com>
2014-09-26 15:57:55 -0400

    Defined classes Warrior Viking and Knight

---
+++ fighters.rb
@@ -0,0 +1,52 @@
+class Warrior
+  attr_accessor :name, :health, :endurance, :armor, :jab_damage, :cross_damage, :hook_damage, :uppercut_damage, :kick_damage
+  def initialize(name, health, endurance, armor, jab_damage, cross_damage, hook_damage, uppercut_damage, kick_damage)
+    @name = name
+    @health = health
+    @endurance = endurance
+    @armor = armor
+    @jab_damage = jab_damage
+    @cross_damage = cross_damage
+    @hook_damage = hook_damage
+    @uppercut_damage = uppercut_damage
+    @kick_damage = kick_damage
+  end
+
+  def jab
+    @health = @health - jab_damage
+  end
+  def cross
+    @health = @health - cross_damage
+  end
+  def hook
+    @health = @health - hook_damage
+  end
+  def uppercut
+    @health = @health - uppercut_damage
+  end
+  def kick
+    @health = @health - uppercut_damage
+  end
+end
+
+class Knight < Warrior
+  def initialize(name, health, endurance, armor, jab_damage, cross_damage, hook_damage, uppercut_damage, kick_damage)
+    super(name, health, endurance, armor, jab_damage, cross_damage, hook_damage, uppercut_damage, kick_damage, axe_damage)
+    @axe_damage = axe_damage
+  end
+
+  def axe_hit
+    @health = @health - axe_damage
+  end
+end
+
+class Viking < Warrior
+  def initialize(name, health, endurance, armor, jab_damage, cross_damage, hook_damage, uppercut_damage, kick_damage)
+    super(name, health, endurance, armor, jab_damage, cross_damage, hook_damage, uppercut_damage, kick_damage, axe_damage)
+    @sword_damage = sword_damage
+  end
+
+  def sword_hit
+    @health = @health - sword_damage
+  end
+end
