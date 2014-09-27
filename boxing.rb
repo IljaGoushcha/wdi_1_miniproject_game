fab8f29e7e5c2330fc73bda688ff6a528ac9c8b7
IljaGoushcha <iljagou@yahoo.com>
2014-09-27 10:40:02 -0400

    upgraded Classes, added fighter generator methods

--- boxing.rb
+++ boxing.rb
@@ -1,40 +1,61 @@
-class Boxer
-  attr_accessor(:health, :power)
-  def initialize(health, power)
+require 'pry'
+# puts generate_fighters(10)[:runners][3].to_string  hang@generalassemb.ly or ga.co
+
+class Runner
+  attr_accessor(:name, :health, :stamina, :power)
+  def initialize(name, health, stamina, power)
+    @name = name
     @health = health
+    @stamina = stamina
     @power = power
   end
   def to_string
-    puts "Boxer"
+    puts "Runner: " + @name
     puts "health: #{@health}"
+    puts "stamina: #{stamina}"
     puts "power: #{@power}"
   end
 end
 
-class KickBoxer
-  attr_accessor(:health, :power)
-  def initialize(health, power)
+class Viking
+  attr_accessor(:name, :health, :stamina, :power)
+  def initialize(name, health, stamina, power)
+    @name = name
     @health = health
+    @stamina = stamina
     @power = power
   end
   def to_string
-    puts "Kick-boxer"
+    puts "Viking: " + @name
     puts "health: #{@health}"
+    puts "stamina: #{stamina}"
     puts "power: #{@power}"
   end
 end
 
-def generate_boxer
-  Boxer.new(rand(50..100), rand(1..50))
+def generate_runner(name, health)
+  Runner.new(name, health, rand(50..100), rand(1..50))
 end
 
-def generate_kickboxer
-  KickBoxer.new(rand(1..50), rand(50..100))
+def generate_viking(name, health)
+  Viking.new(name, health, rand(1..50), rand(50..100))
 end
 
-my_boxer = generate_boxer
-my_boxer.to_string
+def generate_fighters(number)
+  runners = []
+  vikings = []
+  for i in 1..number
+    runners << generate_runner(i.to_s)
+    vikings << generate_viking(i.to_s)
+  end
+  fighters = {runners: runners, viking: vikings}
+end
 
-my_kickboxer = generate_kickboxer
-my_kickboxer.to_string
+def attack(offender, defender)
+
+end
+puts generate_runner("Oleg", 10000).to_string
+
+# puts generate_fighters(10)[:runners].length.to_s
+# puts generate_fighters(10)[:runners][3].to_string
 
